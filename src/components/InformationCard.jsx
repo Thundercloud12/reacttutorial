ab0e3484ce96a4d44d5c4c6d15e40047248caa4b
Thundercloud12 <poojarysaritha064@gmail.com>
2025-03-05 19:51:42 +0530

    Update InformationCard.jsx

--- src/components/InformationCard.jsx
+++ src/components/InformationCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import SwiggyButton from './SwiggyButton'
 
 const InformationCard = ({recipe, onClose}) => {
   return (
  <div className="fixed inset-0 flex items-center justify-center bg-amber-100 bg-opacity-50 z-50">
-      <div className="bg-white relative max-w-md h-150 w-150">
+      <div className="bg-white relative max-w-md h-180 w-150">
         <button 
           onClick={onClose}
           className="absolute top-2 right-2 text-red-600 hover:text-gray-800 text-2xl"
@@ -21,6 +22,10 @@ const InformationCard = ({recipe, onClose}) => {
           <h2 className="text-xl font-bold mb-4">{recipe.strMeal}</h2>
           <p>Details for recipe: {recipe.strInstructions}</p>
           {/* Insert additional detailed information here */}
+          <div className='flex space-x-56'>
+          <p className='font-extrabold' >Too tired to cook this?</p>
+          <SwiggyButton recipename = {recipe.strMeal} />
+        </div>
         </div>
       </div>
     </div>
