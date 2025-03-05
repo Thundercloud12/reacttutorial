bd1c1cffdb55d358bf7c8d74f6be92248b7da851
Thundercloud12 <poojarysaritha064@gmail.com>
2025-03-05 20:01:51 +0530

    Update FavouritePage.jsx

--- src/pages/FavouritePage.jsx
+++ src/pages/FavouritePage.jsx
@@ -6,7 +6,7 @@ const FavoritesPage = () => {
   
 
 	return (
-		<div className='bg-[#faf9fb] flex-1 p-10 min-h-screen'>
+		<div className='bg-[#faf9fb] flex-1 p-10 md:p-10 min-h-screen'>
 			<div className='max-w-screen-lg mx-auto'>
 				<p className='font-bold text-3xl md:text-5xl my-4'>My Favorites</p>
 
